cac024ecc978c2faf8b735d1bf5ddf94d8f219a0
¨EmilioFlores¨ <emilio_wassa@hotmail.com>
2016-04-09 15:26:51 -0500

    redirect shieat

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :set_session_locale
+  before_action :set_session_locale 
 
   def set_session_locale
     I18n.locale = user_signed_in? ? current_user.locale.to_sym : 
     	I18n.default_locale
+  end
+
+  def validate_user
+  	redirect_to new_user_session_path unless current_user
+
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 # Welcome controller. Serves as the user's dashboard
 class WelcomeController < ApplicationController
   def home
+  		redirect_to new_user_session_path unless current_user
+  		
+  	
   end
 end
